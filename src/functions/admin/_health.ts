b3ac7fe59b0262d0c8c0f423a1c4e37d5e9bd811
Toshiek-art <aldomalasomma@proton.me>
2025-09-15 10:48:32 +0000

    fix: restore /functions at repo root and temp no-op middleware

--- src/functions/admin/_health.ts
+++
@@ -1,6 +0,0 @@
-export const onRequest: PagesFunction = async () => {
-  return new Response("ok", { headers: { "content-type": "text/plain" } });
-};
-export const onRequest: PagesFunction = async () => {
-  return new Response("ok", { headers: { "content-type": "text/plain" } });
-};
