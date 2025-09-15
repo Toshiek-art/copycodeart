ca2399228bf7921933733fa0ebba2efdc76879d9
Toshiek-art <aldomalasomma@proton.me>
2025-09-15 10:58:05 +0000

    cleanup functions and add no-op middleware

--- functions/api/hello.ts
+++
@@ -1,5 +0,0 @@
-export const onRequestGet: PagesFunction = async () => {
-  return new Response(JSON.stringify({ message: "Hello from CopyCodeArt API" }), {
-    headers: { "Content-Type": "application/json" }
-  });
-};
